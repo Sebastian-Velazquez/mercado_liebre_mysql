e329b146e4f0b569212a379aa355f3ef49e9c0b8
Sebastian-Velazquez <sergiovelazquez1693@gmail.com>
2023-02-22 10:38:25 -0300

    indentando el codigo

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -3,30 +3,32 @@ let db = require("../database/models")
 //const { Sequelize } = require('sequelize')
 const controlador ={
     index:(req, res)=>{
-        db.Movies.findAll()
-        .then(function(peliculas){
-            res.render("moviesList",{peliculas:peliculas})
-        })
-        .catch(function(error){
-            res.send(error);
-        })
+        db.Movies
+            .findAll()
+            .then(function(peliculas){
+                res.render("moviesList",{peliculas:peliculas})
+            })
+            .catch(function(error){
+                res.send(error);
+            })
     },
     detail:(req,res)=>{
-        db.Movies.findByPk(req.params.id,{//paramrtro del body. id porque pusimos asi en el router
-            include:[{association: "genero"},{association: "actores"}]
-        })
-        .then(pelicula=>{
-            res.render("movieDetail",{pelicula:pelicula})
-        })
-        .catch(function(error){
-            res.send(error);
-        })
+        db.Movies
+            .findByPk(req.params.id,{//paramrtro del body. id porque pusimos asi en el router
+                include:[{association: "genero"},{association: "actores"}]})//asociamos las relaciones de tablas
+            .then(pelicula=>{
+                res.render("movieDetail",{pelicula:pelicula})
+            })
+            .catch(function(error){
+                res.send(error);
+            })
     },
     drama:(req, res)=>{
-        db.Movies.findAll({
-                where:{
-                    genre_id: 3//busca el genero con id 3
-                }
+        db.Movies
+            .findAll({
+                    where:{
+                        genre_id: 3//busca el genero con id 3
+                    }
             })
             .then(function(peliculas){
                 res.render("moviesDrama",{peliculas:peliculas})
@@ -36,7 +38,8 @@ const controlador ={
             })
     },
     top:(req, res)=>{
-        db.Movies.findAll({
+        db.Movies
+            .findAll({
                 where:{
                     rating: {[db.Sequelize.Op.gt] : 8}//gt es mayor que 8
                 },
@@ -64,36 +67,38 @@ const controlador ={
             res.send(error);
         }) */ 
         //
-        db.Genres.findAll()
-        .then(generos=>{
-            res.render("movieCreate",{generos:generos})
-        })
-        .catch(function(error){
-            res.send(error);
-        }); 
-
+        db.Genres
+            .findAll()
+            .then(generos=>{
+                res.render("movieCreate",{generos:generos})
+            })
+            .catch(function(error){
+                res.send(error);
+            }); 
     },
     processCreate:(req, res)=>{
         //validacion
         const resultValidation = validationResult(req);//validacion
         if (resultValidation.errors.length > 0){//resultValidation.errors es un objeto literal//mapped: pasa la variable resultValidation a literiario
-            db.Genres.findAll()
-        .then(function(generos){
-            return res.render("movieCreate",{generos:generos, errors: resultValidation.mapped(), oldData: req.body})
-        })
-        .catch(function(error){
-            res.send(error);
-        })
+            db.Genres
+            .findAll()
+            .then(function(generos){
+                return res.render("movieCreate",{generos:generos, errors: resultValidation.mapped(), oldData: req.body})
+            })
+            .catch(function(error){
+                res.send(error);
+            })
             //return res.render('movieCreate', {errors: resultValidation.mapped(), oldData: req.body }) //Para mostrar los datos bien ingresados
-            }else{//si todo esta bien se guarda
+        }else{//si todo esta bien se guarda
             
-        db.Movies.create({
-            title: req.body.titulo,  //del lado izquierdo es el nombrede la columnas en la base de datos
-            awards: req.body.premio,
-            release_date: req.body.fechaEstreno,
-            genre_id: req.body.genero,
-            length: req.body.duracion,
-            rating:  req.body.rating  //del lado derecho son los nombres de los formularios
+            db.Movies
+                .create({
+                    title: req.body.titulo,  //del lado izquierdo es el nombrede la columnas en la base de datos
+                    awards: req.body.premio,
+                    release_date: req.body.fechaEstreno,
+                    genre_id: req.body.genero,
+                    length: req.body.duracion,
+                    rating:  req.body.rating  //del lado derecho son los nombres de los formularios
         })
         return res.redirect("/movies/list")
         }
@@ -131,28 +136,30 @@ const controlador ={
                 oldData: req.body})
         })
         }else{
-                db.Movies.update({
-            title: req.body.titulo,  //del lado izquierdo es el nombrede la columnas en la base de datos
-            awards: req.body.premio,
-            release_date: req.body.fechaEstreno,
-            genre_id: req.body.genero,
-            length: req.body.duracion,
-            rating:  req.body.rating  //del lado derecho son los nombres de los formularios
-            },{
-                where:{
-                    id: req.params.id
-                }
-            });
-            res.redirect("/movies/list/" + req.params.id)
+            db.Movies
+                .update({
+                    title: req.body.titulo,  //del lado izquierdo es el nombrede la columnas en la base de datos
+                    awards: req.body.premio,
+                    release_date: req.body.fechaEstreno,
+                    genre_id: req.body.genero,
+                    length: req.body.duracion,
+                    rating:  req.body.rating  //del lado derecho son los nombres de los formularios
+                    },{
+                        where:{
+                        id: req.params.id
+                    }
+                });
+                res.redirect("/movies/list/" + req.params.id)
         }
     },
     delete:(req,res)=>{
         //res.send("HOLA")
-        db.Movies.destroy({
-            where:{
-                id: req.params.id
-            }
-        })
+        db.Movies
+            .destroy({
+                where:{
+                    id: req.params.id
+                }
+            })
         res.redirect("/movies/list")
     },
     actorForFilm:(req, res)=>{
@@ -171,10 +178,11 @@ const controlador ={
         });  */
     },
     processActorForFilm:(req, res)=>{
-        db.ActoresPeliculas.create({
-            actor_id: req.body.actor,  //del lado izquierdo es el nombrede la columnas en la base de datos
-            movie_id: req.params.id
-        });
+        db.ActoresPeliculas
+            .create({
+                actor_id: req.body.actor,  //del lado izquierdo  es el nombrede la columnas en la base de datos
+                movie_id: req.params.id
+            });
         return res.redirect("/movies/list/" + req.params.id)
     }
 }
